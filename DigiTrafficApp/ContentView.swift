c2a8ee3d6be589b3bebcfd6d1339ddb17df816c0
Toni <toni.tani@student.saimia.fi>
2020-02-14 02:15:51 +0200

    Initial Commit

---
+++ DigiTrafficApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DigiTrafficApp
+//
+//  Created by Toni Tani on 14.2.2020.
+//  Copyright © 2020 Toni Tani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
