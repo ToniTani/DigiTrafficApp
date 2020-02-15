a01717a08f4d12c60c0f5556cc113340bec2201f
Toni <toni.tani@student.saimia.fi>
2020-02-16 01:43:43 +0200

    Created Camera swift class to test digitraffic api

---
+++ DigiTrafficApp/Models/Camera.swift
@@ -0,0 +1,13 @@
+//
+//  Camera.swift
+//  DigiTrafficApp
+//
+//  Created by Toni Tani on 16.2.2020.
+//  Copyright © 2020 Toni Tani. All rights reserved.
+//
+
+import Foundation
+
+struct Camera: Decodable {
+    let presentationName: String
+}
