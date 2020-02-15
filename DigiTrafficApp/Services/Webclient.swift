831cb950005bcac0defa5cee47b442c143fca66b
Toni <toni.tani@student.saimia.fi>
2020-02-16 01:43:58 +0200

    JSON parsing

--- DigiTrafficApp/Services/Webclient.swift
+++ DigiTrafficApp/Services/Webclient.swift
@@ -10,9 +10,20 @@ import Foundation
 
 class Webclient {
     
-    func get cameraInfo(completion: @escaping ([Camera]?) -> Void){
+    func getCameras(completion: @escaping (([Camera]?) -> Void)) {
+        
         guard let url = URL(string: "https://tie.digitraffic.fi/api/v1/data/camera-data")else{
             fatalError("URL is not correct")
         }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            let cameras = try? JSONDecoder().decode([Camera].self, from: data)
+            cameras == nil ? completion(nil) : completion(cameras)
+            
+        }.resume()
     }
 }
