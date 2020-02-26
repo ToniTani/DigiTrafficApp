70b5dc4c766f11a34020b466a4714fed9ae719a0
Toni <toni.tani@student.saimia.fi>
2020-02-26 10:22:34 +0200

    Fixed Webclient / service to prevent app from crashing when user inputs wrong, null  or empty value.

--- DigiTrafficApp/Services/Webclient.swift
+++ DigiTrafficApp/Services/Webclient.swift
@@ -10,18 +10,29 @@ import Foundation
 
 class Webclient {
     
-    func getCameras(by id: String, completion: @escaping (Camera) -> Void) {
+    func getCameras(by id: String, completion: @escaping ((Camera?) -> Void)) {
         
         guard let url = URL(string: "https://tie.digitraffic.fi/api/v1/data/camera-data/\(id)") else {
             fatalError("URL is not correct")
         }
-                    URLSession.shared.dataTask(with: url) { data, _, _ in
-                        let cam = try! JSONDecoder().decode(Camera.self, from: data!)
-                        DispatchQueue.main.async {
-                            completion(cam)
-                        }
-                    }.resume()
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async {
+                    completion(nil)
                 }
+                return
+            }
+    
+        let cameras = try? JSONDecoder().decode(Camera.self, from: data)
+    
+        DispatchQueue.main.async {
+            completion(cameras)
+    
+            }
+        }.resume()
     }
+}
+
     
 
