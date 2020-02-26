40ad1a484c54b531299985b6c56943a7cfb78e1e
Toni <toni.tani@student.saimia.fi>
2020-02-26 10:24:37 +0200

    Commented out CameraListViewModel

--- DigiTrafficApp/ViewModels/CameraListViewModel.swift
+++ DigiTrafficApp/ViewModels/CameraListViewModel.swift
@@ -6,26 +6,41 @@
 //  Copyright © 2020 Toni Tani. All rights reserved.
 //
 
-/*import Foundation
+/* import Foundation
+import Combine
+
 
 class CameraListViewModel: ObservableObject {
     
+    
+    private var webService: Webclient!
+    @Published var current: Camera?
+    
+    init() {
+        
+        self.webService = Webclient()
+    }
+    
     var searchTerm: String = ""
-    @Published var cameras: [CameraViewModel] = [CameraViewModel]()
     
-    func load(){
-        fetchCameras()
+    var idName: String = ""
+    
+    func search() {
+        if let id = self.idName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            fetchCamera(by: id)
         }
+    }
     
-    private func fetchCameras() {
-        Webclient().getCameras(by: id) { cameras in
-            if let cameras = cameras {
-                DispatchQueue.main.async {
-                    self.cameras = cameras.map(CameraViewModel.init)
-                    
-                }
+    private func fetchCamera(by id: String) {
+        
+        self.webService.getCameras(by: id) { camera in
+        if let camera = camera {
+            DispatchQueue.main.async {
+            self.current = camera
+            }
             }
         }
     }
-} */
-
+    
+}
+*/
